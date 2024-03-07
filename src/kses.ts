390e328925030b9db18fbddc1045343d4707fdc3
jozefini <jozibashaj@gmail.com>
2024-03-07 23:23:15 +0100

    rsc

--- src/kses.ts
+++ src/kses.ts
@@ -16,7 +16,30 @@ export function kses(
   const parser = new DOMParser()
   let htmlDoc = parser.parseFromString(htmlString, 'text/html')
 
-  Array.from(htmlDoc.body.children).forEach((child) =>
+  Array.from(htmlDoc.body.children).forEach(child =>
+    traverseNode(child as Element, allowedTags, allowedProtocols)
+  )
+
+  return htmlDoc.body.innerHTML
+}
+
+export async function ksesServer(
+  htmlString: string,
+  allowedTags: AllowedPostTags = allowedPostTags,
+  allowedProtocols: string[] = ['http', 'https']
+): Promise<string> {
+  if (typeof window !== 'undefined') {
+    throw new Error('import { kses } from @codesync/kses')
+  }
+  if (!validateArguments(htmlString, allowedTags, allowedProtocols)) {
+    return ''
+  }
+
+  const { JSDOM } = await import('jsdom')
+  const parser = new JSDOM(htmlString)
+  let htmlDoc = parser.window.document
+
+  Array.from(htmlDoc.body.children).forEach(child =>
     traverseNode(child as Element, allowedTags, allowedProtocols)
   )
 
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: {
     index: 'src/index.ts',
-    server: 'src/server.ts',
   },
   splitting: true,
   sourcemap: true,
